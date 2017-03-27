6bb9ee651ca5943ee507f9fcaa000a0db681d22e
inti0304 <inti0304@DESKTOP-RT65F4M.home>
2017-03-27 11:49:57 +0200

    fg

--- Agence_CavAle/src/main/webapp/static/scriptsJS/controllers/calendarDemoCtrl.js
+++ Agence_CavAle/src/main/webapp/static/scriptsJS/controllers/calendarDemoCtrl.js
@@ -41,25 +41,65 @@ app.controller('CalendarDemoCtrl', function($scope, visiteProvider, $rootScope,
 				if ($rootScope.visites == undefined) {
 					$rootScope.visites = [];
 				}
+				// methode supprimer avec un lien
+				$scope.deleteLien = function(id) {
+					visiteProvider.deleteVisite(id, function(callback) {
+						if (callback != undefined && callback != "") {
+
+							visiteProvider.findAllVisite(function(callback_visite) {
+								if (callback_visite != undefined
+										&& callback_visite != "") {
+									$scope.visites = callback_visite.data;
+									$location.path("getAllVisite")
+								}
+							});
+						}
+					})
+				}
+				// declarer la visite dans le rootscope
+				$rootScope.visiteForm = {
+					id : undefined,
+					date : "",
+					client : null,
+					agent : null,
+					bienALouer : null,
+					bienAAcheter : null
+				}
+				// appelé la methode du provider avec le lien de l'index
+				$scope.updateLien = function(visite) {
+					$rootScope.visiteForm.id = visite.id;
+					$rootScope.visiteForm.date = visite.date;
+					$rootScope.visiteForm.client = visite.client;
+					$rootScope.visiteForm.agent = visite.agent;
+					$rootScope.visiteForm.bienALouer = visite.bienALouer;
+					$rootScope.visiteForm.bienAAcheter = visite.bienAAcheter;
+					$location.path("updateVisite")
+				}		
+				
 
 				$rootScope.visites.forEach(function(visite) {
 
 					var date = new Date(visite.date);
+					var client = visite.client;
+					var bienALouer = visite.bienALouer;
+					var bienAAcheter = visite.bienAAcheter;
 					
 					var startTime = new Date(date.getFullYear(), date
-							.getMonth(), date.getDate()+date.getDay(), date.getHours(), date
+							.getMonth(), date.getDate(), date.getHours(), date
 							.getMinutes());
 					
 					var endTime = new Date(date.getFullYear(), date.getMonth(),
-							date.getDate()+date.getDay(), date.getHours() + 1, date
+							date.getDate(), date.getHours() + 1, date
 									.getMinutes());
 					
 
 					events.push({
 						title : 'Visite',
-
 						startTime : startTime,
 						endTime : endTime,
+						client : client,
+						bienALouer : bienALouer,
+						bienAAcheter : bienAAcheter,
 						allDay : false
 
 					});
@@ -72,46 +112,46 @@ app.controller('CalendarDemoCtrl', function($scope, visiteProvider, $rootScope,
 
 	// cette fonction etait l'exemple qui permettait la création d'event
 	// aléatoir placé dans le calendrier
-	function createRandomEvents() {
-		var events = [];
-		for (var i = 0; i < 50; i += 1) {
-			var date = new Date();
-			var eventType = Math.floor(Math.random() * 2);
-			var startDay = Math.floor(Math.random() * 90) - 45;
-			var endDay = Math.floor(Math.random() * 2) + startDay;
-			var startTime;
-			var endTime;
-			if (eventType === 0) {
-				startTime = new Date(Date.UTC(date.getUTCFullYear(), date
-						.getUTCMonth(), date.getUTCDate() + startDay));
-				if (endDay === startDay) {
-					endDay += 1;
-				}
-				endTime = new Date(Date.UTC(date.getUTCFullYear(), date
-						.getUTCMonth(), date.getUTCDate() + endDay));
-				events.push({
-					title : 'All Day - ' + i,
-					startTime : startTime,
-					endTime : endTime,
-					allDay : true
-				});
-			} else {
-				var startMinute = Math.floor(Math.random() * 24 * 60);
-				var endMinute = Math.floor(Math.random() * 180) + startMinute;
-				startTime = new Date(date.getFullYear(), date.getMonth(), date
-						.getDate()
-						+ startDay, 0, date.getMinutes() + startMinute);
-				endTime = new Date(date.getFullYear(), date.getMonth(), date
-						.getDate()
-						+ endDay, 0, date.getMinutes() + endMinute);
-				events.push({
-					title : 'Event - ' + i,
-					startTime : startTime,
-					endTime : endTime,
-					allDay : false
-				});
-			}
-		}
-		return events;
-	}
+//	function createRandomEvents() {
+//		var events = [];
+//		for (var i = 0; i < 50; i += 1) {
+//			var date = new Date();
+//			var eventType = Math.floor(Math.random() * 2);
+//			var startDay = Math.floor(Math.random() * 90) - 45;
+//			var endDay = Math.floor(Math.random() * 2) + startDay;
+//			var startTime;
+//			var endTime;
+//			if (eventType === 0) {
+//				startTime = new Date(Date.UTC(date.getUTCFullYear(), date
+//						.getUTCMonth(), date.getUTCDate() + startDay));
+//				if (endDay === startDay) {
+//					endDay += 1;
+//				}
+//				endTime = new Date(Date.UTC(date.getUTCFullYear(), date
+//						.getUTCMonth(), date.getUTCDate() + endDay));
+//				events.push({
+//					title : 'All Day - ' + i,
+//					startTime : startTime,
+//					endTime : endTime,
+//					allDay : true
+//				});
+//			} else {
+//				var startMinute = Math.floor(Math.random() * 24 * 60);
+//				var endMinute = Math.floor(Math.random() * 180) + startMinute;
+//				startTime = new Date(date.getFullYear(), date.getMonth(), date
+//						.getDate()
+//						+ startDay, 0, date.getMinutes() + startMinute);
+//				endTime = new Date(date.getFullYear(), date.getMonth(), date
+//						.getDate()
+//						+ endDay, 0, date.getMinutes() + endMinute);
+//				events.push({
+//					title : 'Event - ' + i,
+//					startTime : startTime,
+//					endTime : endTime,
+//					allDay : false
+//				});
+//			}
+//		}
+//		return events;
+//	}
 })
